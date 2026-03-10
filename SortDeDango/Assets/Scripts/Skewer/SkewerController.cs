425b29fafe8eac05072d88fbaa8d0f55e2d3e4aa
yUnoming <yuknownme@gmail.com>
2026-03-11 03:44:12 +0900

    Add: 串の基本スクリプト作成
    ◆内容
    - 串で使う基本スクリプトの作成
    
    〇SkewerController.cs
    > 串のメインスクリプト
    > いづれController化する可能性が高いので名称にControllerを追加
    
    〇SkewerState.cs
    > 串の状態を表すEnum

---
+++ SortDeDango/Assets/Scripts/Skewer/SkewerController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SkewerController : MonoBehaviour
+{
+    [Tooltip("串の現在の状態")]
+    private SkewerState currentState;
+
+    /// <summary>
+    /// 状態を変更    </summary>
+    /// <param name="nextState">
+    /// 変更先の状態  </param>
+    public void ChangeState(SkewerState nextState){ currentState = nextState; }
+}
---
+++ SortDeDango/Assets/Scripts/Skewer/SkewerState.cs
@@ -0,0 +1,13 @@
+﻿public enum SkewerState
+{
+    None,
+    /// <summary>
+    /// 何も刺さっていない    </summary>
+    Empty,
+    /// <summary>
+    /// 団子が刺さっている    </summary>
+    Stack,
+    /// <summary>
+    /// 串が完成している    </summary>
+    Complete,
+}
