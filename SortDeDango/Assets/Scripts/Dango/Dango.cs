943abd6dabb58be7895fcd672a78bbdb494ab817
yUnoming <yuknownme@gmail.com>
2026-03-11 03:04:15 +0900

    Add: 団子の基本スクリプト作成
    ◆内容
    - 団子で使う基本スクリプトの作成
    
    〇Dango.cs
    - 団子のメインスクリプト
    - 団子の色情報を持つ
    
    〇DangoColor.cs
    > 団子の色を表すEnum

---
+++ SortDeDango/Assets/Scripts/Dango/Dango.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Dango : MonoBehaviour
+{
+    [SerializeField, Tooltip("団子の色")]
+    private DangoColor dangoColor;
+
+    public void SetColor(DangoColor color){ dangoColor = color; }
+}
