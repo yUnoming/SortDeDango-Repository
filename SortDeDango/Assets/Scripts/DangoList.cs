9af11971ba2464b801406841bdd951aa84e3cb60
yUnoming <yuknownme@gmail.com>
2026-03-17 08:06:16 +0900

    Add: ステージデータの実装
    ◆概要
    - ステージ生成に向けて、ステージ構成を保持するデータの実装
    
    ◆内容
    - ステージデータクラス"StageData"を実装
      - 串の総数
      - 串ごとの団子色設定リスト

---
+++ SortDeDango/Assets/Scripts/DangoList.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class DangoList
+{
+    [Header("リスト0が一番下の団子")]
+    public List<DangoColor> dangoColors = new List<DangoColor>();
+}
---
+++ SortDeDango/Assets/Scripts/StageData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StageData", menuName = "SortDeDango/StageData")]
+public class StageData : ScriptableObject
+{
+    [Tooltip("串の総数")]
+    public int totalSkewers;
+    [Header("串の総数分リストを追加\nリスト0が左上の串に対応。そこからＺ方向に進行")]
+    [Tooltip("団子リスト")]
+    public List<DangoList> dangoLists;
+}
