ed9d34d42dcd382e90a77b1ae51825b38837c28c
yUnoming <yuknownme@gmail.com>
2026-03-18 10:09:50 +0900

    Add: 団子色をモデルに反映する処理を実装
    ◆概要
    - ステージ生成時に渡される団子色を、団子モデルに反映する処理を実装
    
    ◆内容
    - Dangoに団子色を設定、モデルへ反映する関数 SetColor() を実装
    - StageGeneratorで生成した団子に対して SetColor() を呼び出すように変更
    - 団子の色別プレハブを削除

--- SortDeDango/Assets/Scripts/Dango/Dango.cs
+++ SortDeDango/Assets/Scripts/Dango/Dango.cs
@@ -1,8 +1,21 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Dango : MonoBehaviour
 {
     [SerializeField, Tooltip("団子の色")]
     private DangoColor color;
-    public DangoColor Color { get { return color; } set { color = value; } }
+    public DangoColor Color { get { return color; }}
+    [SerializeField]
+    private MeshRenderer meshRenderer;
+    [SerializeField, Header("DangoColorの順番通りにセット")]
+    private List<Material> colorMaterials = new List<Material>();
+
+    /// <summary>
+    /// 団子の色をセット    </summary>
+    public void SetColor(DangoColor newColor)
+    {
+        color = newColor;
+        meshRenderer.material = colorMaterials[(int)color - 1];
+    }
 }
