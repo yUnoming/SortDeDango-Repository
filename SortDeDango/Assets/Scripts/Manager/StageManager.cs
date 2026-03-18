ef04842a5f0bbb98afb0fb5bdcc63c24d677b688
yUnoming <yuknownme@gmail.com>
2026-03-18 16:26:00 +0900

    Add: ステージ管理クラスの実装
    ◆概要
    - ステージ管理クラス"StageManager"を実装
      - ステージ番号およびステージデータの管理を担当
      - ステージ遷移処理は行わない
    
    ◆内容
    - StageManagerに以下処理を実装
      - シングルトン化
      - ステージ番号およびステージデータリストの保持
      - 現在のステージデータを取得するプロパティ"CurrentStageData"
      - ステージ番号を設定する関数"SetStage()"
      - 次のステージへ進む関数"MoveStage()"
      - 前のステージへ戻る関数"ReturnStage()"

---
+++ SortDeDango/Assets/Scripts/Manager/StageManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    [SerializeField, Tooltip("ステージ番号")]
+    private int stageNumber = 1;
+    [SerializeField, Tooltip("ステージデータリスト")]
+    private List<StageData> stageDataList = new List<StageData>();
+
+    /// <summary>
+    /// インスタンス    </summary>
+    private static StageManager instance;
+    public static StageManager Instance
+    {
+        get
+        {
+            // インスタンスが無ければ、グローバルマネージャーをシーン内に生成
+            if (instance == null)
+            {
+                GameObject prefab = Resources.Load<GameObject>("GlobalManagersVariant");
+                GameObject obj = Instantiate(prefab);
+                instance = obj.GetComponentInChildren<StageManager>();
+            }
+            return instance;
+        }
+    }
+    /// <summary>
+    /// 現在のステージデータ  </summary>
+    public StageData CurrentStageData => stageDataList[stageNumber - 1];
+
+    protected void Awake()
+    {
+        // 重複確認
+        if (instance != null && instance != this)
+        {
+            Destroy(transform.parent.gameObject);
+            return;
+        }
+        // インスタンス化
+        else if (instance == null) instance = this;
+    }
+
+    /// <summary>
+    /// ステージ番号をセット    </summary>
+    /// <param name="newStageNumber">
+    /// セットするステージ番号    </param>
+    public void SetStage(int newStageNumber) { stageNumber = newStageNumber; }
+    /// <summary>
+    /// ステージ番号を一つ進める    </summary>
+    public void MoveStage()
+    {
+        stageNumber++;
+        if (stageNumber > stageDataList.Count) stageNumber = 1;
+    }
+    /// <summary>
+    /// ステージ番号を一つ戻す    </summary>
+    public void ReturnStage()
+    { 
+        stageNumber--;
+        if (stageNumber <= 0) stageNumber = stageDataList.Count;
+    }
+}
