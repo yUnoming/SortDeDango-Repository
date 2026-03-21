6140da2109c47f0c49cd12a511aa598bc551f7d1
yUnoming <yuknownme@gmail.com>
2026-03-21 12:13:13 +0900

    Add: Undo機能の実装
    ◆概要
    - 団子の移動を一手前に戻すUndo機能を実装
      - Uキーで実行
    
    ◆内容
    - 団子の移動データを保持するクラス"MoveData"を実装
    - GameplayControllerに処理を実装
      - 移動データを保持するリスト"moveDataList"
      - 一手戻す処理を行う関数"Undo()"

---
+++ SortDeDango/Assets/Scripts/MoveData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MoveData
+{
+    [Tooltip("移動元の串")]
+    public SkewerController from;
+    [Tooltip("移動先の串")]
+    public SkewerController to;
+    [Tooltip("移動させた団子")]
+    public Dango dango;
+
+    public MoveData(SkewerController from, SkewerController to, Dango dango)
+    {
+        this.from = from;
+        this.to = to;
+        this.dango = dango;
+    }
+}
