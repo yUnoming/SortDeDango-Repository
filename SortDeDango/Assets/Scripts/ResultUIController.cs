9775542c14fc65888ae90a4223c44527f2f9e522
yUnoming <yuknownme@gmail.com>
2026-03-21 14:49:49 +0900

    Add: リザルトUIの実装
    ◆概要
    - ゲームクリア時に表示するリザルトUIを実装
      - クリアテキスト
      - Nextボタン: 次のステージへ進む
    - Nextボタン押下で次のステージへ進む処理を実装
    
    ◆内容
    - ResultUIRootプレハブを作成
      - リザルトUIをひとまとめにしたプレハブ
      - GameplayCanvases/OverlayCanvas 配下に配置
    - ResultUIControllerを実装
      - リザルトUIの表示 / 非表示
      - Nextボタン押下時のイベント設定
    - GameplayManagerに処理を実装
      - Nextボタン押下時の処理を行う関数"HandleNextClicked()"
      - ResultUIControllerにイベントを渡す処理

---
+++ SortDeDango/Assets/Scripts/ResultUIController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultUIController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject content;
+    [SerializeField]
+    private Button nextButton;
+
+    [Tooltip("Nextボタン押下時のイベント")]
+    public event Action OnNextClicked;
+
+    private void Awake()
+    {
+        nextButton.onClick.AddListener(() => OnNextClicked?.Invoke());
+    }
+
+    /// <summary>
+    /// UI表示    </summary>
+    public void Show() { content.SetActive(true); }
+    /// <summary>
+    /// UI非表示    </summary>
+    public void Hide() { content.SetActive(false); }
+}
