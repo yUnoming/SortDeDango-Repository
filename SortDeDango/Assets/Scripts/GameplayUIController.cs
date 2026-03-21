7fca1a01a13bcaa486d434d935c0df855cf210fd
yUnoming <yuknownme@gmail.com>
2026-03-21 17:04:16 +0900

    Add: ゲームプレイUIの実装
    ◆概要
    - ゲームプレイ中に表示するUIを実装
      - ステージ番号
      - Restartボタン(ステージを再スタート)
      - Undoボタン(一手戻す)
    
    ◆内容
    - GameplayUIRootプレハブを作成
      - ゲームプレイUIをひとまとめにしたプレハブ
      - GameplayCanvases/HUDCanvas 配下に配置
    - GameplayUIControllerを実装
      - ゲームプレイUIの表示 / 非表示
      - RestartボタンおよびUndoボタン押下時のイベント設定
      - ステージ番号を設定する関数"SetStageNumber()"
    - GameplayControllerに処理を実装
      - Restartボタン押下時の処理を行う関数 "HandleRestartClicked()"
      - Undoボタン押下時の処理を行う関数 "HandleUndoClicked()"
      - GameplayUIControllerにイベントを渡す処理
      - GameplayUIControllerのSetStageNumber()を実行

---
+++ SortDeDango/Assets/Scripts/GameplayUIController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameplayUIController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject content;
+    [SerializeField]
+    private TextMeshProUGUI stageNumberTMP;
+    [SerializeField]
+    private Button restartButton;
+    [SerializeField]
+    private Button undoButton;
+
+    [Tooltip("Restartボタン押下時のイベント")]
+    public event Action onRestartClicked;
+    [Tooltip("Undoボタン押下時のイベント")]
+    public event Action onUndoClicked;
+
+
+    private void Awake()
+    {
+        restartButton.onClick.AddListener(() => onRestartClicked?.Invoke());
+        undoButton.onClick.AddListener(() => onUndoClicked?.Invoke());
+    }
+
+    /// <summary>
+    /// UI表示    </summary>
+    public void Show() { content.SetActive(true); }
+    /// <summary>
+    /// UI非表示    </summary>
+    public void Hide() { content.SetActive(false); }
+    /// <summary>
+    /// ステージ番号を設定    </summary>
+    public void SetStageNumber(int stageNumber)
+    {
+        stageNumberTMP.text = $"Stage {stageNumber}";
+    }
+}
