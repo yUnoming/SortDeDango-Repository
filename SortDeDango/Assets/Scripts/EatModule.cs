6549c85aecdff5d7584ea4ebc9852d5e25d34afd
yUnoming <yuknownme@gmail.com>
2026-03-24 08:24:19 +0900

    Add: 食べるアクションの実装
    ◆概要
    - 食べるアクションの処理を行う"EatModule"を実装
    - Eキー入力後、対象の団子位置に対応した数字キー入力で実行
    
    ◆内容
    - EatModuleに食べるアクションに必要な処理を実装
      - 食べられる最大数を表す値
      - 残り回数を表す値
      - 数字キー入力で対応した位置の団子を削除する処理
      - 食べるアクションが実行可能か判定する処理
    - GameplayControllerに食べるアクション入力処理を実装
    - GameplayControllerプレハブにEatModuleコンポーネントを追加

---
+++ SortDeDango/Assets/Scripts/EatModule.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EatModule : MonoBehaviour
+{
+    [SerializeField, Tooltip("食べられる最大回数")]
+    private int maxEatCount;
+    [Tooltip("食べられる残り回数")]
+    private int remainingEatCount;
+
+    private void Awake()
+    {
+        remainingEatCount = maxEatCount;
+    }
+
+    /// <summary>
+    /// 団子を食べられるか判定    </summary>
+    /// <param name="selectingSkewer">
+    /// 判定対象の串    </param>
+    public bool CanEat(SkewerController targetSkewer)
+    {
+        if (targetSkewer == null || remainingEatCount == 0) return false;
+        return targetSkewer.HasDango();
+    }
+    /// <summary>
+    /// 指定した串をから団子食べる処理を行い、成功判定を返す    </summary>
+    /// <param name="targetSkewer">
+    /// 食べる対象の串    </param>
+    /// <param name="eatDangoIndex">
+    /// 食べる団子のインデックス番号    </param>
+    public bool TryEat(SkewerController targetSkewer, int eatDangoIndex)
+    {
+        // 団子を食べることに成功
+        Dango eatenDango = targetSkewer.RemoveDangoAt(eatDangoIndex);
+        if (eatenDango != null)
+        {
+            Destroy(eatenDango.gameObject);    // オブジェクト削除
+            remainingEatCount--;
+            return true;
+        }
+        // 団子を食べることに失敗
+        return false;
+    }
+    /// <summary>
+    /// 団子を食べる処理の制御    </summary>
+    /// <param name="targetSkewer">
+    /// 食べる対象の串    </param>
+    public IEnumerator EatSequence(SkewerController targetSkewer)
+    {
+        // 入力されたキー入力を元に、食べる団子を指定
+        int eatDangoIndex = 0;
+        while (true)
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                eatDangoIndex = 1;
+                break;
+            }
+            else if(Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                eatDangoIndex = 2;
+                break;
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                eatDangoIndex = 3;
+                break;
+            }
+            yield return null;
+        }
+        // 食べるのを試みる
+        TryEat(targetSkewer, eatDangoIndex);
+    }
+}
