d6e88ea6b34e3c82a7d9f9ca1841e5a3d55a9f77
yUnoming <yuknownme@gmail.com>
2026-03-24 12:47:24 +0900

    Add: 串の生成処理の実装
    ◆概要
    - 新たな串を一定間隔で生成する処理を実装
    
    ◆内容
    - 串の生成処理を行うクラス"SkewerSpawner"を実装
    - 串の生成パターン管理用クラス"SkewerSpawnTable"を実装

---
+++ SortDeDango/Assets/Scripts/Skewer/SkewerSpawnTable.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SkewerSpawnTable", menuName = "SortDeDango/SkewerSpawnTable")]
+public class SkewerSpawnTable : ScriptableObject
+{
+    public List<DangoList> entries = new List<DangoList>();
+}
---
+++ SortDeDango/Assets/Scripts/Skewer/SkewerSpawner.cs
@@ -0,0 +1,52 @@
+﻿using UnityEditor.Overlays;
+using UnityEngine;
+
+public class SkewerSpawner : MonoBehaviour
+{
+    [SerializeField, Tooltip("生成間隔")]
+    private float spawnInterval;
+    [SerializeField, Tooltip("生成テーブル")]
+    private SkewerSpawnTable spawnTable;
+    [SerializeField, Tooltip("串の格納先")]
+    Transform skewerRoot;
+    [SerializeField, Tooltip("串プレハブ")]
+    private GameObject skewerPrefab;
+    [SerializeField, Tooltip("団子プレハブ")]
+    private GameObject dangoPrefab;
+
+    [Tooltip("生成までの時間計測")]
+    private float spawnTimer;
+
+    private void Update()
+    {
+        // 一定時間経過で生成
+        spawnTimer += Time.deltaTime;
+        if(spawnTimer >= spawnInterval)
+        {
+            Spawn();
+            spawnTimer -= spawnInterval;
+        }
+    }
+
+    private void Spawn()
+    {
+        // 生成番号を乱数取得
+        int randomIndex = Random.Range(0, spawnTable.entries.Count);
+
+        // 串の生成・初期設定
+        GameObject skewerObj = Instantiate(skewerPrefab);
+        SkewerController skewer = skewerObj.GetComponent<SkewerController>();
+        skewer.transform.parent = skewerRoot;   // 親の設定
+
+        // 生成するの団子色分のループ
+        foreach (DangoColor dangoColor in spawnTable.entries[randomIndex].dangoColors)
+        {
+            // 団子の生成・初期設定
+            GameObject dangoObj = Instantiate(dangoPrefab);
+            Dango dango = dangoObj.GetComponent<Dango>();
+            dango.SetColor(dangoColor);                     // 団子色の設定
+            skewer.AddDango(dango.GetComponent<Dango>());   // 団子の追加
+            skewer.SetTopDangoPosition(dango);              // 団子の配置
+        }
+    }
+}
