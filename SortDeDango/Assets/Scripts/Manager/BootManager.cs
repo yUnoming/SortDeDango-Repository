02907bd8ee97025296a17c42b3d56b3e1c674165
yUnoming <yuknownme@gmail.com>
2026-04-09 01:27:46 +0900

    Add: 起動時のセットアップ用シーンの作成
    ◆概要
    - ゲーム起動時にセットアップを行うためのシーンを作成
    
    ◆内容
    - `BootScene` / `BootManager`を作成
    - BootManagerに起動後の初期シーンへ遷移する処理を実装

---
+++ SortDeDango/Assets/Scripts/Manager/BootManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class BootManager : SceneManagerBase<BootManager>
+{
+    protected override void StateRunning()
+    {
+        // 起動時のセットアップ等はここで行う
+        ChangeScene(SceneType.Gameplay, true, "GameplayScene");
+    }
+}
