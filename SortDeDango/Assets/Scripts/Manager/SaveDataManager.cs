98528168db5b0237ebd62def05058f738cb92b28
yUnoming <yuknownme@gmail.com>
2026-04-13 05:28:37 +0900

    Add: セーブ処理の基盤実装
    ◆概要
    - Continueとステージ選択機能実装に向けて、進行状況を保持するセーブデータを実装
    
    ◆内容
    - `SaveData`クラスを実装
      - 現在到達しているステージ番号を保持
      - 最後に遊んだステージ番号を保持
    - `SaveDataManager`クラスを実装
      - セーブデータの保存処理
      - セーブデータの読み込み処理
    -  `SaveDataManager`をアタッチしたプレハブを実装
      - `GlobalManagers`に配置

---
+++ SortDeDango/Assets/Scripts/Manager/SaveDataManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class SaveDataManager : MonoBehaviour
+{
+    private static SaveDataManager instance;
+    public static SaveDataManager Instance => instance;
+
+    private SaveData currentSaveData;
+    private const string ReachedStageKey = "ReachedStageIndex";
+    private const string LastPlayedStageKey = "LastPlayedStageIndex";
+
+    private void Awake()
+    {
+        // 重複確認
+        if (instance != null && instance != this)
+        {
+            Destroy(transform.parent.gameObject);
+            return;
+        }
+        // シングルトン化
+        else if (instance == null) instance = this;
+    }
+    private void OnApplicationQuit()
+    {
+        Save(currentSaveData);
+    }
+
+    /// <summary>
+    /// セーブデータ作成    </summary>
+    public SaveData CreateSaveData()
+    {
+        currentSaveData = new SaveData();
+        return currentSaveData;
+    }
+    /// <summary>
+    /// セーブ    </summary>
+    public void Save(SaveData saveData)
+    {
+        PlayerPrefs.SetInt(ReachedStageKey, saveData.reachedStageIndex);
+        PlayerPrefs.SetInt(LastPlayedStageKey, saveData.lastPlayedStageIndex);
+        PlayerPrefs.Save();
+    }
+    /// <summary>
+    /// ロード    </summary>
+    public SaveData Load()
+    {
+        currentSaveData = new SaveData();
+        currentSaveData.reachedStageIndex = PlayerPrefs.GetInt(ReachedStageKey, 1);
+        currentSaveData.lastPlayedStageIndex = PlayerPrefs.GetInt(LastPlayedStageKey, 1);
+
+        return currentSaveData;
+    }
+}
