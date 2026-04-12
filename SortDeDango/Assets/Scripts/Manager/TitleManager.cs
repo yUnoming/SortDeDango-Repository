d3d53657da49cf7321ea976714300aaffc5ea0fb
yUnoming <yuknownme@gmail.com>
2026-04-13 07:50:19 +0900

    Update: NewGameボタンのセーブ対応
    ◆概要
    - NewGameが押された際にセーブデータを初期化する処理を実装
    
    ◆内容
    - `TitleUIController`クラスを実装
    - `TitleUIController`にNewGameボタン押下時のイベント設定を実装
    - `TitleManager`にNewGameボタン押下時のイベントを渡す処理を実装
    - `TitleManager`にNewGameボタン押下時の処理を実装
      - `SaveDataManager`のセーブデータ作成処理を実行
      - 開始ステージ番号を初期ステージに設定
      - ゲームシーンへ遷移

--- SortDeDango/Assets/Scripts/Manager/TitleManager.cs
+++ SortDeDango/Assets/Scripts/Manager/TitleManager.cs
@@ -2,4 +2,19 @@
 
 public class TitleManager : SceneManagerBase<TitleManager>
 {
+    protected override void StateInit()
+    {
+        TitleUIController titleUI = FindAnyObjectByType<TitleUIController>();
+        titleUI.onNewGameClicked += HandleNewGameClicked;
+        base.StateInit();
+    }
+
+    /// <summary>
+    /// NewGameボタン押下時の処理    </summary>
+    private void HandleNewGameClicked()
+    {
+        SaveData newSaveData = SaveDataManager.Instance.CreateSaveData();
+        StageManager.Instance.SetStage(newSaveData.reachedStageIndex);
+        ChangeScene(SceneType.Gameplay, true, "GameplayScene");
+    }
 }
---
+++ SortDeDango/Assets/Scripts/TitleUIController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleUIController : MonoBehaviour
+{
+    [SerializeField]
+    private Button newGameButton;
+
+    [Tooltip("NewGameボタン押下時のイベント")]
+    public event Action onNewGameClicked;
+
+    private void Awake()
+    {
+        newGameButton.onClick.AddListener(() => onNewGameClicked?.Invoke());
+    }
+}
