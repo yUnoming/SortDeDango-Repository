70437290ad32ef164aecd04ef1f979851f687148
yUnoming <yuknownme@gmail.com>
2026-05-22 23:17:35 +0900

    Update: Undoボタンの押下状態更新
    ◆概要
    - 行動履歴の有無によって、Undoボタンの押下状態を更新する処理の実装
    - Undoボタン専用クラスの実装
    
    ◆内容
    - `UndoButton`クラスの実装
      - `ButtonBase`を継承
      - 押下状態とボタンクリック処理のづれにより生じるSEの不具合を解消するため
    - `GameplayUIController`に、Undoボタンの押下状態更新関数を実装
    - `GameplayController`の行動履歴保存後に`GameplayUIController`のUndoボタンの押下状態更新関数を実行する処理を実装

---
+++ SortDeDango/Assets/Scripts/Button/UndoButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UndoButton : ButtonBase
+{
+    [Tooltip("前フレームの押下状態")]
+    private bool previousInteractable;
+
+    private void LateUpdate()
+    {
+        previousInteractable = button.interactable;
+    }
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        if (button != null && (button.interactable || previousInteractable)) AudioManager.Instance.PlaySE(buttonClickSE, false);
+        else AudioManager.Instance.PlaySE(buttonClickInvalidSE, false);
+    }
+}
