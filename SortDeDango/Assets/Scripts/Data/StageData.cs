24c782e2106bf43690886a2a8866525b9daa219a
yUnoming <yuknownme@gmail.com>
2026-05-31 22:03:30 +0900

    Add: チュートリアルの表示追加
    ◆概要
    - ステージにチュートリアルテキストを表示する機能の追加
    
    ◆内容
    - チュートリアル表示用プレハブ`TutorialWindow`を作成
    - メッセージウィンドウの管理スクリプト`MessageWindowController.cs`を実装
    - `GameplayController.cs`,`GameplayUIController.cs`にチュートリアル表示までの導線となる処理を実装
    - `StageData.cs`にチュートリアルの表示内容を設定する項目を追加

--- SortDeDango/Assets/Scripts/Data/StageData.cs
+++ SortDeDango/Assets/Scripts/Data/StageData.cs
@@ -13,6 +13,8 @@ public class StageData : ScriptableObject
     [Header("串の総数分リストを追加\nリスト0が左上の串に対応。そこからＺ方向に進行")]
     [Tooltip("団子リスト")]
     public List<DangoList> dangoLists;
+    [Tooltip("チュートリアルの表示内容"), TextArea]
+    public string tutorialMessage;
 
     [HideInInspector, Tooltip("串の総数")]
     public int SkewerCount => dangoLists.Count;
---
+++ SortDeDango/Assets/Scripts/UI/MessageWindowController.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class MessageWindowController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject windowPanel;
+    [SerializeField]
+    private TextMeshProUGUI messageText;
+
+    /// <summary>
+    /// ウィンドウを開く    </summary>
+    public void ShowWindow() { windowPanel.SetActive(true); }
+    /// <summary>
+    /// ウィンドウを閉じる    </summary>
+    public void CloseWindow() { windowPanel.SetActive(false); }
+    /// <summary>
+    /// 表示する文字列のセット    </summary>
+    public void SetMessage(string message) { messageText.text = message; }
+}
