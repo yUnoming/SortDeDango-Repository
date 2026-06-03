d4dc8b6f50c18b01fc42272b1d28873f3079d840
yUnoming <yuknownme@gmail.com>
2026-06-03 19:58:12 +0900

    Add: BGM追加
    ## 概要
    - シーン内で再生するBGMの導入
    - BGMを再生する処理の実装
    
    ## 実装内容
    - タイトルとステージ選択画面で再生するBGM`bgm_default_01.mp3`を導入し、`AudioData`化
    - ゲームプレイ画面で再生するBGM`bgm_gameplay_01.mp3`を導入し、`AudioData`化
    - 専用シーンマネージャー内にBGMを再生する処理を実装
    
    ## 動作確認
    - タイトル画面とステージ選択画面で同一のBGMが再生されること
    - ゲームプレイ画面で専用BGMが再生されること

--- SortDeDango/Assets/Scripts/Manager/StageSelectManager.cs
+++ SortDeDango/Assets/Scripts/Manager/StageSelectManager.cs
@@ -1,10 +1,21 @@
-﻿public class StageSelectManager : SceneManagerBase<StageSelectManager>
+﻿using UnityEngine;
+
+public class StageSelectManager : SceneManagerBase<StageSelectManager>
 {
+    [SerializeField]
+    private AudioData bgm;
+
     protected override void StateInit()
     {
         // 現在のセーブデータから値を取得し、ステージ選択ボタンのロックを解除
         StageSelectUIController stageSelectUI = FindAnyObjectByType<StageSelectUIController>();
         stageSelectUI.UpdateStageSelectButtons(SaveDataManager.Instance.CurrentSaveData.reachedStageIndex);
+
         base.StateInit();
+    }
+    protected override void StateStart()
+    {
+        AudioManager.Instance.PlayBGM(bgm);
+        base.StateStart();
     }
 }
