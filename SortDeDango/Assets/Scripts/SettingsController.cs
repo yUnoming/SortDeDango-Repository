5e432aea94ff106ac1beaaa5473ecfa6a0df8edf
yUnoming <yuknownme@gmail.com>
2026-06-04 20:30:06 +0900

    Update: 起動時にロードした設定を反映
    ## 概要
    - 設定内容の反映が設定画面を開いたタイミングだったので、ロード後に反映されるように変更
    
    ## 内容
    - `SettingsController.Awake()`で設定ファイルのロード後にゲームに反映する処理を実装
    
    ## 動作確認
    - 前回設定した内容が、ゲーム起動後に反映されていること

--- SortDeDango/Assets/Scripts/SettingsController.cs
+++ SortDeDango/Assets/Scripts/SettingsController.cs
@@ -1,52 +1,45 @@
-﻿using System.IO;
+﻿using System.Collections;
+using System.IO;
+using NUnit.Framework.Constraints;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SettingsController : MonoBehaviour
 {
-    [SerializeField]
-    private Slider masterVolumeSlider;
-    [SerializeField]
-    private Slider bgmVolumeSlider;
-    [SerializeField]
-    private Slider seVolumeSlider;
-
+    [Tooltip("設定データのファイルパス")]
     private const string SettingsFilePath = "settings.json";
+    [Tooltip("設定データ")]
     private SettingsData settingsData;
+    public SettingsData SettingsData => settingsData;
 
     private void Awake()
-    {
-        Setup();
-
-        masterVolumeSlider.onValueChanged.AddListener((value) => SetAudioVolume(AudioType.Master, value));
-        bgmVolumeSlider.onValueChanged.AddListener((value) => SetAudioVolume(AudioType.BGM, value));
-        seVolumeSlider.onValueChanged.AddListener((value) => SetAudioVolume(AudioType.SE, value));
-    }
-    
-    private void Setup()
     {
         // 設定内容の読み込み
         settingsData = Load();
         // 読み込んだ設定内容を反映
-        masterVolumeSlider.value = settingsData.masterVolume;
-        bgmVolumeSlider.value = settingsData.bgmVolume;
-        seVolumeSlider.value = settingsData.seVolume;
+        SetAudioVolume(AudioType.Master, settingsData.masterVolume);
+        SetAudioVolume(AudioType.BGM, settingsData.bgmVolume);
+        SetAudioVolume(AudioType.SE, settingsData.seVolume);
     }
 
     /// <summary>
     /// 音量ボリュームを設定    </summary>
     public void SetAudioVolume(AudioType type, float volume)
     {
+        // 実際の音量ボリュームに設定
         AudioManager.Instance.SetVolume(type, volume);
+        // 設定データに保存
+        switch (type)
+        {
+            case AudioType.Master: settingsData.masterVolume = volume;  break;
+            case AudioType.BGM: settingsData.bgmVolume = volume;    break;
+            case AudioType.SE: settingsData.seVolume = volume;  break;
+        }
     }
     /// <summary>
     /// 設定内容の保存    </summary>
     public void Save()
     {
-        settingsData.masterVolume = masterVolumeSlider.value;
-        settingsData.bgmVolume = bgmVolumeSlider.value;
-        settingsData.seVolume = seVolumeSlider.value;
-
         string json = JsonUtility.ToJson(settingsData);
         string path = Path.Combine(Application.persistentDataPath, SettingsFilePath);
         File.WriteAllText(path, json);
