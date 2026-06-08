bbe19f623c7e2c5e6c11dc7189d161bf5c17b5d1
yUnoming <yuknownme@gmail.com>
2026-06-08 16:00:02 +0900

    Fix: 削除後もエフェクトが残り続ける不具合
    ## 概要
    - エフェクト発生後インスタンス削除の処理を行っていたはずが、削除されない不具合を解消
    
    ## 実装内容
    - `EffectBase.Destroy()`でオブジェクトごと削除されるように変更
    - `DangoEatEffect.Play()`を`PlayOnce()`に変更
    
    ## 動作確認
    - エフェクト発生後、インスタンスごとエフェクトが削除されること

--- SortDeDango/Assets/Scripts/Dango/DangoEatEffect.cs
+++ SortDeDango/Assets/Scripts/Dango/DangoEatEffect.cs
@@ -6,11 +6,11 @@ public class DangoEatEffect : EffectBase
     /// エフェクト再生    </summary>
     /// <param name="material">
     /// エフェクトの色指定用のマテリアル    </param>
-    public void Play(Material material)
+    public void PlayOnce(Material material)
     {
         // パーティクルの色を変更してから再生
         ParticleSystem.MainModule main = effect.main;
         main.startColor = new ParticleSystem.MinMaxGradient(material.color);
-        base.Play();
+        base.PlayOnce();
     }
 }
